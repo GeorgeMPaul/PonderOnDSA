84ce840a4ebc39b62ac8b51169b7fd36c3705791
George Mathew Paul <georgemathewpaul2021@gmail.com>
2025-01-07 23:56:57 +0530

    Create ProductOfArrayExceptItself.cs

---
+++ ProductOfArrayExceptItself.cs
@@ -0,0 +1,33 @@
+public class Solution
+{
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        int n = nums.Length;
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
+        int[] ans = new int[n];
+
+        prefix[0] = 1;
+        suffix[n - 1] = 1;
+
+        // Fill prefix array
+        for (int i = 1; i < n; i++)
+        {
+            prefix[i] = prefix[i - 1] * nums[i - 1];
+        }
+
+        // Fill suffix array
+        for (int i = n - 2; i >= 0; i--)
+        {
+            suffix[i] = suffix[i + 1] * nums[i + 1];
+        }
+
+        // Calculate the result
+        for (int i = 0; i < n; i++)
+        {
+            ans[i] = prefix[i] * suffix[i];
+        }
+
+        return ans;
+    }
+}
